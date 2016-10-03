3a8cb136de5f4ab2a39eb039bfb8f90a7e6b45e7
Yukinaga2 <yukinaga2@MacBook-Pro-3.local>
2016-10-03 14:58:36 +0900

    Initial Commit

---
+++ GPUCalc1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPUCalc1
+//
+//  Created by Yukinaga2 on 2016/10/03.
+//  Copyright © 2016年 Yukinaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
